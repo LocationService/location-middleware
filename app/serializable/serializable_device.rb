b889011231fb835acfd312ecb3ef5524f76cd4a6
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2017-09-24 18:51:21 +0300

    Add link to locations from device

--- app/serializable/serializable_device.rb
+++ app/serializable/serializable_device.rb
@@ -1,4 +1,14 @@
 class SerializableDevice < JSONAPI::Serializable::Resource
   type 'devices'
   attributes :did, :user_agent
+
+  has_many :locations do
+    link :related do
+      @url_helpers.api_v1_device_locations_path(@object.id)
+    end
+
+    meta do
+      { count: @object.locations.count }
+    end
+  end
 end
