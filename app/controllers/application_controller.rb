b9eaf3abd58065cdfa20831c51304621a5647dba
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2017-11-14 14:14:26 +0200

    Add TODO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,3 +2,6 @@ class ApplicationController < ActionController::API
   include Knock::Authenticable
   before_action :authenticate_user
 end
+
+# TODO: add device register endpoint
+# TODO: add device auth endpoint
