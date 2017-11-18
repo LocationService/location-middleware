25d8a0d7cc2accd5e5b4107b47680bac16e37309
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2017-11-19 00:11:11 +0200

    Improve device authenticate

--- app/controllers/devices/authenticate_controller.rb
+++ app/controllers/devices/authenticate_controller.rb
@@ -1,27 +1,30 @@
 module Devices
   class AuthenticateController < ApplicationController
     before_action :authenticate!
+    attr_reader :current_device
 
     private
 
     def authenticate!
-      unauthorize! unless current_device
+      unauthorize unless authenticate_device
     end
 
-    def unauthorize!
+    def unauthorize
       head(:unauthorized)
     end
 
-    def current_device
-      return @current_device if @current_device
-      device_id = entity_token&.payload&.dig("device_id")
+    def authenticate_device
+      entity_token = build_entity_token
+      return unless entity_token
+      device_id = entity_token.payload["device_id"]
       return unless device_id
       @current_device = Device.find(device_id)
     end
 
-    def entity_token
+    def build_entity_token
       return unless token
-      Knock::AuthToken.new(token: token) rescue nil
+      Knock::AuthToken.new(token: token)
+    rescue JWT::IncorrectAlgorithm, JWT::DecodeError
     end
 
     def token
