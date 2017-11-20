8eda3046e7e252878efb57bd6de41d66b1cc344b
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2017-11-20 19:59:16 +0200

    Render devices

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::DevicesController < AuthenticateController
   def index
-    render jsonapi: current_user.devices
+    render jsonapi: Device.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :devices
-
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
