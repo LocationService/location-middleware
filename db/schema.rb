aac8a70345f73e403c988538b38911e4829d6969
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2018-02-25 21:04:37 +0200

    Add timezone to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128141133) do
+ActiveRecord::Schema.define(version: 20180214165638) do
 
   create_table "devices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "device_id", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171128141133) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "time_zone", default: "Europe/Kiev", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
