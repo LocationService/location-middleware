740d2a71c9408913d67f141afd2693b545e74346
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2018-02-25 20:55:43 +0200

    Add time_zone to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
+  def time_zone
+    ActiveSupport::TimeZone[super] || Time.zone
+  end
 end
